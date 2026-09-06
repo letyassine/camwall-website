3e1cdc4a8fea0d69affff1186b38c8708f5c1400
yassine <yassinehaimouch@proton.me>
2026-09-06 18:14:04 +0400

    feat: code source link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+import { FaGithub } from "react-icons/fa";
+
 function Navbar() {
   return (
     <nav className="flex items-center justify-between py-4">
@@ -17,7 +19,10 @@ function Navbar() {
           <a href="/">Download</a>
         </li> */}
       </ul>
-      <div>
+      <div className="flex items-center justify-center gap-2">
+        <a href="https://github.com/letyassine/callwall">
+          <FaGithub size={24} className="hover:text-foreground/80" />
+        </a>
         <a
           href="https://github.com/sponsors/letyassine/"
           className="text-white text-sm font-normal bg-linear-to-b from-sky-400 to-sky-500 shadow-[0px_0px_1px_1px_rgba(255,255,255,0.06)_inset,0px_1.5px_2px_0px_rgba(0,0,0,0.1),0px_0px_0px_1px_var(--color-sky-500)] rounded-full px-3 py-1 relative z-10 w-fit whitespace-nowrap"
